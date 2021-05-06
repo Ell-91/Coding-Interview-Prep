b473b8d94ae48eb04b69f327582a75a946cf1649
Ell-91 <cdsherman@ucdavis.edu>
2021-05-05 22:55:58 -0700

    added delete()"
    exit
    
    git add linkedList.py

--- Data Structure Implementation/linkedList.py
+++ Data Structure Implementation/linkedList.py
@@ -64,15 +64,32 @@ class linkedList:
   # 1. Node to be deleted is head
   # 2. Node to be deleted is not head
   def delete(self,data_key):
-  
-  current_node = self.head #set curret node to be the head of the list
-  #first check if the head node is the node to be deleted
-  if prev_node:
-    print("The node does not exist")
-    return
+    current_node = self.head #set curret node to be the head of the list
+    
+    #Case 1: delete the head node
+    #first check if the head node is the node to be deleted
+    if current_node and current_node.data == data_key:  #if current_node is not None (list is not empty)  and the data is equal to the key, then go through steps
+      
+    #setting the head equal to the next node in the list
+      self.head = current_node.next
+      current_node = None   #delete the previous node 
 
-  self.head = curr_node.next
+    #Case 2: delete a node that is not the head of the list
+    #Find the nodes before and after the node we want to delete
+    #current_node = self.head
+    prev_node = None
+    while current_node and current_node.data != data_key:  #while the head node is not None and while the data fields of the nodes we encounter aren't equal to the key
+      prev_node = current_node #current_node = self.head
+      current_node = current_node.next  #moves the head point along
+    
+    #if current_node = NULL then the element isn't in the list
+    if current_node is None: #The element in the list is not present
+      return 
 
+    prev_node.next = current_node.next
+    current_node = None
+
+  def delete_position(self, position, data):
 
 llist = linkedList()
 llist.append("A")
@@ -84,6 +101,8 @@ llist.append("D")
 #print(llist.head.data) #---> A
 #print(llist.head.next.data) #---> B
 
-llist.insert_arbitrarily(llist.head.next, "E")
-llist.print_list()
+#llist.insert_arbitrarily(llist.head.next, "E")
+#llist.print_list()
 
+llist.delete("B")
+llist.print_list()
